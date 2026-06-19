96ff1a83d73aa76ee034491bdac35ce15e28535c
sanjana <sanjana10@gmail.com>
2026-06-19 21:21:38 +0530

    Flask E-Commerce Project

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "myseceretkey"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+@app.route('/')
+def home():
+    username = session.get('username')
+    return render_template('index.html', username=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(
+            email=email,
+            password=password
+        ).first()
+
+        if user:
+            session['username'] = user.name
+            return redirect('/')
+
+        return "Invalid Email or Password"
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User(
+            name=name,
+            email=email,
+            password=password
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/login')
+
+    return render_template('register.html')
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+@app.route('/checkout')
+def checkout():
+    return render_template("checkout.html")
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
